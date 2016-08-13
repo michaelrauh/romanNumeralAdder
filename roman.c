a85d8620a3403f6c1e148f4f3aec3b36299c928f
michaelrauh <michaelrauh@ymail.com>
2016-08-13 14:55:44 -0400

    roman char to arabic now searches for correct character and then looks it up

--- roman.c
+++ roman.c
@@ -1,26 +1,16 @@
 #include "roman.h"
 
 int roman_char_to_arabic(char* x){
-	if (*x == 'I'){
-		return 1;
-	}
-	else if (*x == 'V'){
-		return 5;
-	}
-	else if (*x == 'X'){
-		return 10;
-	}
-	else if (*x == 'L'){
-		return 50;
-	}
-	else if (*x == 'C'){
-		return 100;
-	}
-	else if (*x == 'D'){
-		return 500;
-	}
-	else{
-		return 1000;
+	char numerals[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+	int arabics[] = {1, 5, 10, 50, 100, 500, 1000};
+	int size = sizeof(numerals)/sizeof(numerals[0]);
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		if (numerals[i] == *x)
+		{
+			return arabics[i];
+		}
 	}
 }
 
