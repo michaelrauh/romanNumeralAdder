436ba9c9bdb25d4d7ddc59080bd4c931f877dd9f
michaelrauh <michaelrauh@ymail.com>
2016-08-14 19:16:09 -0400

    added handling of adding a bad to a good numeral

--- roman_converter.c
+++ roman_converter.c
@@ -113,6 +113,10 @@ const char* add_roman(const char* x, const char* y){
 	int l = roman_to_arabic(x);
 	int r = roman_to_arabic(y);
 
+	if (l == ERROR || r == ERROR){
+		return INVALID;
+	}
+
 	int total = l + r;
 
 	return arabic_to_roman(total);
