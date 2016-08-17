1b8428a5daca18876900b06121a7ea917eb121cc
michaelrauh <michaelrauh@ymail.com>
2016-08-16 20:19:31 -0400

    split header into two files to cover roman and roman_converter. Removing access to helper methods

---
+++ roman.h
@@ -0,0 +1,4 @@
+#define ERROR 0
+#define INVALID "Z"
+const char* add_roman(const char* x, const char* y);
+const char* subtract_roman(const char* x, const char* y);
--- roman_converter.h
+++ roman_converter.h
@@ -1,10 +1,4 @@
 #define ERROR 0
 #define INVALID "Z"
-int roman_char_to_arabic(const char x);
-int max_repeat(const int x);
-int invalid_subtract(const int cur, const int prev);
 int roman_to_arabic(const char* x);
-const char* arabic_to_max_roman_pattern(int x);
 const char* arabic_to_roman(int x);
-const char* add_roman(const char* x, const char* y);
-const char* subtract_roman(const char* x, const char* y);
